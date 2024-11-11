4918686e5b1323d7be32a9b77fa793adc679d23d
dsmith1149 <dantayvionsmith@gmail.com>
2024-11-11 04:42:12 -0600

    Edit Events Exercise

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,19 +1,34 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
 public class Event {
 private int id;
 private static int nextId = 1;
-
+@NotBlank(message = "Name is required.")
+@Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
 private String name;
+
+@Size(max = 500, message = "Description too long!")
 private String description;
 
-    public Event(String name, String description) {
+@NotBlank(message = "Email is required.")
+@Email(message = "Invalid email. Try again.")
+private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
+    }
+    public Event(){
+
     }
 
     public int getId() {
@@ -34,6 +49,14 @@ private String description;
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public @Email String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(@Email String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     @Override
