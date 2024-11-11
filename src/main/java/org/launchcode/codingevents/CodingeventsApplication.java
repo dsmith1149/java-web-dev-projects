9e39343f4a91256953e5e7095c375bf87fec652b
dsmith1149 <dantayvionsmith@gmail.com>
2024-11-11 03:14:25 -0600

    Edit Events Exercise

--- src/main/java/org/launchcode/codingevents/CodingeventsApplication.java
+++ src/main/java/org/launchcode/codingevents/CodingeventsApplication.java
@@ -3,11 +3,14 @@ package org.launchcode.codingevents;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+
 @SpringBootApplication
 public class CodingeventsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodingeventsApplication.class, args);
-	}
 
+
+	}
 }
